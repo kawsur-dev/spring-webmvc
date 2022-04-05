5991fbb3ab551656753b4b5388379e3ef5163a75
kawsur <kawsur@aiub.edu>
2022-04-05 10:14:28 +0600

    spring mvc part 1

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    /*public String showForm() {
+
+    }*/
+}
